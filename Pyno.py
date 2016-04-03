0d44194ca39b9b4f5dd80c9ce074f967028ec869
Fyodor Shchukin <honix@users.noreply.github.com>
2016-04-03 11:28:52 +0300

    Load default config if it wrong
    btw there is a bug when user delete some elements..

--- Pyno.py
+++ Pyno.py
@@ -6,10 +6,15 @@ import window
 
 if __name__ == '__main__':
     print('Loading...')
+    
     config = pyglet.gl.Config(double_buffer=False, depth_size=0,
                               stencil_size=0, aux_buffers=0,
                               samples=1)
-    pwindow = window.PynoWindow(config=config)
+    try:
+        pwindow = window.PynoWindow(config=config)
+    except:
+        # if config is crashed run more default one
+        pwindow = window.PynoWindow(pyglet.gl.Config())
 
     pyglet.app.run()
     # cProfile.run("pyglet.app.run()")
